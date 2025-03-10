9eefdac6a16a8e588272d2d85e195dd31f3c35d3
mikucodelk <mikucodelk@hotmail.com>
2025-03-10 18:02:35 +0800

    feat: add draggable

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import KfcFormDesigner from '@koodo-fuoomu/form-designer'
 // import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 
 const app = createApp(App)
-
+app.use(ElementPlus)
 app.use(KfcFormDesigner)
 
 app.mount('#app')
--- packages/form-designer/src/form-designer.vue
+++ packages/form-designer/src/form-designer.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { VueDraggable } from 'vue-draggable-plus'
 import { Icon } from '@iconify/vue'
 import { defineProps, ref } from 'vue'
 defineOptions({
@@ -11,12 +12,32 @@ defineProps({
 const activeMenu = ref('component')
 
 const menus = new Map([['component', 'component']])
+const components = ref([
+  {
+    title: '基本组件',
+    components: [
+      { label: '文本框', value: 'span' },
+      { label: '主要内容', value: 'strong' }
+    ]
+  },
+  {
+    title: '表单组件',
+    components: [
+      { label: '输入框', value: 'el-input' },
+      { label: '多行文本', value: 'textarea' }
+    ]
+  }
+])
+const onClone = res => {
+  console.log(res)
+}
+const designerList = ref([])
 </script>
 
 <template>
   <article class="box-border h-full flex flex-col bg-#edeff3">
     <header class="box-border flex-none h-52px bg-white mb-2px px-16px py-8px flex justify-between items-center">
-      <h1 class="box-border text-18px">KOODO FUOOMU</h1>
+      <h1 class="box-border text-24px">KOODO FUOOMU</h1>
       <section>
         <slot name="toolbar"></slot>
       </section>
@@ -30,14 +51,52 @@ const menus = new Map([['component', 'component']])
           >
             <icon
               icon="hugeicons:menu-square"
-              class="color-#666"
+              class="color-#666 text-24px"
               :class="{ '!color-#000': activeMenu === menus.get('component') }"
             ></icon>
           </li>
         </ol>
-        <section class="w-260px bg-white"></section>
+        <section class="w-260px bg-white">
+          <main class="box-border p-12px">
+            <vue-draggable
+              v-for="item in components"
+              :key="item.title"
+              v-model="item.components"
+              target=".component-list"
+              :animation="150"
+              :group="{ name: 'component', pull: 'clone', put: false }"
+              :sort="false"
+              @clone="onClone"
+            >
+              <h2 class="text-16px mb-8px font-bold">{{ item.title }}</h2>
+              <article class="component-list grid grid-cols-2 gap-12px mb-8px text-14px">
+                <section
+                  v-for="component in item.components"
+                  class="component-item cursor-pointer list-none py-4px text-center border border-#666 border-solid rounded-4px"
+                  :key="component.value"
+                >
+                  {{ component.label }}
+                </section>
+              </article>
+            </vue-draggable>
+          </main>
+        </section>
       </aside>
-      <article class="flex-auto bg-white m-16px"></article>
+      <article class="flex-auto bg-white m-16px">
+        <vue-draggable
+          class="h-full"
+          v-model="designerList"
+          :animation="150"
+          group="component"
+        >
+          <section
+            v-for="(item, index) in designerList"
+            :key="index"
+          >
+            <component :is="item.value"></component>
+          </section>
+        </vue-draggable>
+      </article>
       <aside class="flex-none bg-white w-300px">
         <header></header>
       </aside>
